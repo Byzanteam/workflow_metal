7e3097ddffe872314af4d84271b61db7871c6289
Phil Chen <06fahchen@gmail.com>
2020-03-11 11:41:34 +0800

    add dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule WorkflowMetal.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -22,7 +25,8 @@ defmodule WorkflowMetal.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
     ]
   end
 end
