b018aadfd1621b457a856f43de79dd9fbc835040
Phil Chen <06fahchen@gmail.com>
2020-03-15 18:20:03 +0800

    fix credo breaks

--- test/workflow_metal/application_test.exs
+++ test/workflow_metal/application_test.exs
@@ -5,19 +5,18 @@ defmodule WorkflowMetal.ApplicationTest do
     use WorkflowMetal.Application, name: __MODULE__.TestApplication
   end
 
+  alias TestApplication
+  alias WorkflowMetal.Application.Config
+
   test "build an application" do
     start_supervised(DummyApplication)
 
-    assert WorkflowMetal.Application.Config.get(
-             __MODULE__.DummyApplication.TestApplication,
-             :registry
-           )
+    assert Config.get(TestApplication, :registry)
   end
 
   test "create workflow" do
     start_supervised(DummyApplication)
 
-    assert {:ok, _pid} =
-             DummyApplication.create_workflow(DummyApplication.TestApplication, workflow_id: 123)
+    assert {:ok, _pid} = DummyApplication.create_workflow(TestApplication, workflow_id: 123)
   end
 end
