d4a758e9f1eb322120527ea8b3589ada0f548f76
Phil Chen <06fahchen@gmail.com>
2020-03-27 11:32:58 +0800

    tweak schema

--- lib/workflow_metal/storage/schema/token.ex
+++ lib/workflow_metal/storage/schema/token.ex
@@ -1,37 +1,26 @@
 defmodule WorkflowMetal.Storage.Schema.Token do
   @moduledoc false
 
-  @enforce_keys [:id, :workflow_id, :case_id, :state, :produced_at]
+  @enforce_keys [:state, :workflow_id, :case_id]
   defstruct [
-    :id,
     :state,
     :workflow_id,
-    :place_id,
     :case_id,
-    :locked_workitem_id,
-    :produced_at,
-    :locked_at,
-    :canceled_at,
-    :consumed_at
+    :place_id,
+    :locked_workitem_id
   ]
 
-  @type id :: term()
-  @type state :: :free | :locked | :canceled | :finished
+  @type state :: :free | :locked | :consumed
   @type workflow_id :: WorkflowMetal.Storage.Schema.Workflow.id()
   @type place_id :: WorkflowMetal.Storage.Schema.Place.id()
   @type case_id :: WorkflowMetal.Storage.Schema.Case.id()
   @type workitem_id :: WorkflowMetal.Storage.Schema.Workitem.id()
 
   @type t() :: %__MODULE__{
-          id: id,
           state: state,
           workflow_id: workflow_id,
           place_id: place_id,
           case_id: case_id,
-          locked_workitem_id: workitem_id,
-          produced_at: NaiveDateTime.t(),
-          locked_at: NaiveDateTime.t(),
-          canceled_at: NaiveDateTime.t(),
-          consumed_at: NaiveDateTime.t()
+          locked_workitem_id: workitem_id
         }
 end
--- lib/workflow_metal/storage/schema/transition.ex
+++ lib/workflow_metal/storage/schema/transition.ex
@@ -7,16 +7,22 @@ defmodule WorkflowMetal.Storage.Schema.Transition do
   defstruct [
     :id,
     :workflow_id,
+    :join_type,
+    :split_type,
     :executer,
     executer_params: %{}
   ]
 
   @type id :: term()
   @type workflow_id :: WorkflowMetal.Storage.Schema.Workflow.id()
+  @type join_type :: :none | :and
+  @type split_type :: :none | :and
 
   @type t() :: %__MODULE__{
           id: id,
           workflow_id: workflow_id,
+          join_type: join_type,
+          split_type: split_type,
           executer: module(),
           executer_params: map()
         }
