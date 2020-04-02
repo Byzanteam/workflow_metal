37f83f09c124f3c5cab374201687c393aa6e2e57
Phil Chen <06fahchen@gmail.com>
2020-04-02 12:46:36 +0800

    remove fetch api from workflow

--- lib/workflow_metal/workflow/workflow.ex
+++ lib/workflow_metal/workflow/workflow.ex
@@ -7,29 +7,13 @@ defmodule WorkflowMetal.Workflow.Workflow do
 
   defstruct [
     :application,
-    :workflow_id,
-    :place_table,
-    :transition_table,
-    :arc_table
+    :workflow_id
   ]
 
   @type application :: WorkflowMetal.Application.t()
-  @type workflow :: WorkflowMetal.Storage.Schema.Workflow.t()
 
   @type workflow_id :: WorkflowMetal.Storage.Schema.Workflow.id()
   @type workflow_identifier :: {application, workflow_id}
-
-  @type place_id :: WorkflowMetal.Storage.Schema.Place.id()
-  @type place_schema :: WorkflowMetal.Storage.Schema.Place.t()
-
-  @type transition_id :: WorkflowMetal.Storage.Schema.Transition.id()
-  @type transition_schema :: WorkflowMetal.Storage.Schema.Transition.t()
-
-  @type arc_schema :: WorkflowMetal.Storage.Schema.Arc.t()
-
-  @type arc_direction :: WorkflowMetal.Storage.Schema.Arc.direction()
-
-  @arc_directions [:in, :out]
 
   @doc false
   @spec start_link(workflow_identifier) :: GenServer.on_start()
@@ -55,249 +39,16 @@ defmodule WorkflowMetal.Workflow.Workflow do
     WorkflowMetal.Registration.via_tuple(application, name(workflow_id))
   end
 
-  @doc """
-  Retrive a transistion.
-  """
-  @spec fetch_transition(GenServer.server(), transition_id) ::
-          {:ok, transition_schema} | {:error, term()}
-  def fetch_transition(workflow_server, transition_id) do
-    GenServer.call(workflow_server, {:fetch_transition, transition_id})
-  end
-
-  @doc """
-  Retrive transistions of a place.
-  """
-  @spec fetch_transitions(GenServer.server(), place_id, arc_direction) ::
-          {:ok, [transition_schema]} | {:error, term()}
-  def fetch_transitions(workflow_server, place_id, direction) do
-    GenServer.call(workflow_server, {:fetch_transitions, place_id, direction})
-  end
-
-  @doc """
-  Retrive places of a transition.
-  """
-  @spec fetch_places(GenServer.server(), transition_id, arc_direction) ::
-          {:ok, [place_schema]} | {:error, term()}
-  def fetch_places(workflow_server, transition_id, direction) do
-    GenServer.call(workflow_server, {:fetch_places, transition_id, direction})
-  end
-
-  @doc """
-  Retrive the `:start` place.
-  """
-  @spec fetch_place(GenServer.server(), place_id | :start | :end) ::
-          {:ok, place_schema} | {:error, term()}
-  def fetch_place(workflow_server, place_id) do
-    GenServer.call(workflow_server, {:fetch_place, place_id})
-  end
-
-  @doc """
-  Retrieve arcs of a transition
-  """
-  @spec fetch_arcs_with_places(GenServer.server(), transition_id, arc_direction) ::
-          {:ok, [{arc_schema, place_schema}]} | {:error, term()}
-  def fetch_arcs_with_places(workflow_server, transition_id, direction) do
-    GenServer.call(workflow_server, {:fetch_arcs_with_places, transition_id, direction})
-  end
-
   # Server (callbacks)
 
   @impl true
   def init({application, workflow_id}) do
-    place_table = :ets.new(:place_table, [:set, :private])
-    transition_table = :ets.new(:transition_table, [:set, :private])
-    arc_table = :ets.new(:arc_table, [:bag, :private])
-
     {
       :ok,
       %__MODULE__{
         application: application,
-        workflow_id: workflow_id,
-        place_table: place_table,
-        transition_table: transition_table,
-        arc_table: arc_table
-      },
-      {:continue, :rebuild_from_storage}
+        workflow_id: workflow_id
+      }
     }
   end
-
-  @impl true
-  def handle_continue(:rebuild_from_storage, %__MODULE__{} = state) do
-    %{
-      application: application,
-      workflow_id: workflow_id
-    } = state
-
-    case WorkflowMetal.Storage.fetch_workflow(application, workflow_id) do
-      {:ok, workflow_schema} ->
-        %{
-          places: places,
-          transitions: transitions,
-          arcs: arcs
-        } = workflow_schema
-
-        with(
-          {:ok, state} <- insert_places(places, state),
-          {:ok, state} <- insert_transitions(transitions, state),
-          {:ok, state} <- insert_arcs(arcs, state)
-        ) do
-          {:noreply, state}
-        else
-          error ->
-            error
-        end
-
-      {:error, reason} ->
-        {:stop, reason, state}
-    end
-  end
-
-  @impl true
-  def handle_call({:fetch_transition, transition_id}, _from, %__MODULE__{} = state) do
-    %{transition_table: transition_table} = state
-
-    reply =
-      case :ets.select(transition_table, [{{transition_id, :"$1"}, [], [:"$1"]}]) do
-        [transition] -> {:ok, transition}
-        _ -> {:error, :transition_not_found}
-      end
-
-    {:reply, reply, state}
-  end
-
-  @impl true
-  def handle_call({:fetch_transitions, place_id, direction}, _from, %__MODULE__{} = state)
-      when direction in @arc_directions do
-    %{arc_table: arc_table} = state
-
-    transitions =
-      :ets.select(
-        arc_table,
-        [{{{place_id, :_, direction}, :_, :"$1", :_}, [], [:"$1"]}]
-      )
-
-    {:reply, {:ok, transitions}, state}
-  end
-
-  @impl true
-  def handle_call({:fetch_places, transition_id, direction}, _from, %__MODULE__{} = state)
-      when direction in @arc_directions do
-    direction = reversed_direction(direction)
-
-    %{arc_table: arc_table} = state
-
-    places =
-      :ets.select(
-        arc_table,
-        [{{{:_, transition_id, direction}, :"$1", :_, :_}, [], [:"$1"]}]
-      )
-
-    {:reply, {:ok, places}, state}
-  end
-
-  @impl true
-  def handle_call({:fetch_place, place_id}, _from, %__MODULE__{} = state) do
-    %{
-      place_table: place_table
-    } = state
-
-    match_spec =
-      if place_id in [:start, :end] do
-        [{{:_, place_id, :"$1"}, [], [:"$1"]}]
-      else
-        [{{place_id, :_, :"$1"}, [], [:"$1"]}]
-      end
-
-    place_table
-    |> :ets.select(match_spec)
-    |> case do
-      [place] ->
-        {:reply, {:ok, place}, state}
-
-      _ ->
-        {:reply, {:ok, nil}, state}
-    end
-  end
-
-  @impl true
-  def handle_call(
-        {:fetch_arcs_with_places, transition_id, direction},
-        _from,
-        %__MODULE__{} = state
-      )
-      when direction in @arc_directions do
-    %{arc_table: arc_table} = state
-    direction = reversed_direction(direction)
-
-    arcs =
-      :ets.select(
-        arc_table,
-        [{{{:_, transition_id, direction}, :"$1", :_, :"$2"}, [], [{:"$2", :"$1"}]}]
-      )
-
-    {:reply, {:ok, arcs}, state}
-  end
-
-  defp insert_places(places, %__MODULE__{} = state) do
-    %{place_table: table} = state
-
-    Enum.each(places, fn place ->
-      :ets.insert(table, {place.id, place.type, place})
-    end)
-
-    {:ok, state}
-  end
-
-  defp insert_transitions(transitions, %__MODULE__{} = state) do
-    %{transition_table: table} = state
-
-    Enum.each(transitions, fn transition ->
-      :ets.insert(table, {transition.id, transition})
-    end)
-
-    {:ok, state}
-  end
-
-  defp insert_arcs(arcs, %__MODULE__{} = state) do
-    %{
-      arc_table: arc_table,
-      place_table: place_table,
-      transition_table: transition_table
-    } = state
-
-    get_place = fn place_id ->
-      place_table
-      |> :ets.select([{{place_id, :_, :"$1"}, [], [:"$1"]}])
-      |> hd()
-    end
-
-    get_transition = fn transition_id ->
-      transition_table
-      |> :ets.select([{{transition_id, :"$1"}, [], [:"$1"]}])
-      |> hd()
-    end
-
-    Enum.each(arcs, fn arc ->
-      %{
-        place_id: place_id,
-        transition_id: transition_id,
-        direction: direction
-      } = arc
-
-      :ets.insert(
-        arc_table,
-        {
-          {place_id, transition_id, direction},
-          get_place.(place_id),
-          get_transition.(transition_id),
-          arc
-        }
-      )
-    end)
-
-    {:ok, state}
-  end
-
-  defp reversed_direction(:in), do: :out
-  defp reversed_direction(:out), do: :in
 end
