b590fe2e76fae32c77c4ac426c744cccdaa13dcb
Phil Chen <06fahchen@gmail.com>
2020-04-03 21:05:41 +0800

    remove Vertex folder

--- lib/workflow_metal/vertex/supervisor.ex
+++
@@ -1,5 +0,0 @@
-defmodule WorkflowMetal.Vertex.Supervisor do
-  @moduledoc """
-  `DynamicSupervisor` to supervise all vertices of a workflow.
-  """
-end
--- lib/workflow_metal/vertex/task.ex
+++
@@ -1,5 +0,0 @@
-defmodule WorkflowMetal.Vertex.Task do
-  @moduledoc """
-  `GenServer` process to present `Task` vertex.
-  """
-end
