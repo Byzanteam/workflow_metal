d8deba7fa58821096d712209a2377ef0e8c97406
Phil Chen <06fahchen@gmail.com>
2020-07-10 11:05:21 +0800

    refactor: adopt typedstruct in token

--- lib/workflow_metal/storage/schema/token.ex
+++ lib/workflow_metal/storage/schema/token.ex
@@ -4,28 +4,9 @@ defmodule WorkflowMetal.Storage.Schema.Token do
   `:termination` the last token.
   """
 
+  use TypedStruct
+
   alias WorkflowMetal.Storage.Schema
-
-  @enforce_keys [
-    :id,
-    :state,
-    :workflow_id,
-    :case_id,
-    :place_id,
-    :produced_by_task_id
-  ]
-
-  defstruct [
-    :id,
-    :workflow_id,
-    :case_id,
-    :place_id,
-    :produced_by_task_id,
-    :locked_by_task_id,
-    :consumed_by_task_id,
-    :payload,
-    state: :free
-  ]
 
   @type id :: term()
   @type state :: :free | :locked | :consumed
@@ -36,39 +17,36 @@ defmodule WorkflowMetal.Storage.Schema.Token do
   @type case_id :: Schema.Case.id()
   @type task_id :: Schema.Task.id()
 
-  @type t() :: %__MODULE__{
-          id: id,
-          state: state,
-          workflow_id: workflow_id,
-          case_id: case_id,
-          place_id: place_id,
-          produced_by_task_id: task_id,
-          locked_by_task_id: task_id,
-          consumed_by_task_id: task_id,
-          payload: payload
-        }
+  typedstruct enforce: true do
+    field :id, id()
+    field :state, state(), default: :free
+    field :payload, payload(), enforce: false
+
+    field :workflow_id, workflow_id()
+    field :case_id, case_id()
+    field :place_id, place_id()
+
+    field :produced_by_task_id, task_id()
+    field :locked_by_task_id, task_id(), enforce: false
+    field :consumed_by_task_id, task_id(), enforce: false
+  end
 
   alias __MODULE__
 
   defmodule Params do
     @moduledoc false
 
-    @enforce_keys [:workflow_id, :case_id, :place_id, :produced_by_task_id, :payload]
-    defstruct [
-      :workflow_id,
-      :case_id,
-      :place_id,
-      :produced_by_task_id,
-      :payload
-    ]
+    use TypedStruct
 
-    @type t() :: %__MODULE__{
-            workflow_id: Token.workflow_id(),
-            case_id: Token.case_id(),
-            place_id: Token.place_id(),
-            # `:genesis` stands for genesis token
-            produced_by_task_id: Token.task_id() | :genesis,
-            payload: Token.payload()
-          }
+    typedstruct enforce: true do
+      field :payload, Token.payload()
+
+      field :workflow_id, Token.workflow_id()
+      field :case_id, Token.case_id()
+      field :place_id, Token.place_id()
+
+      # `:genesis` stands for genesis token
+      field :produced_by_task_id, Token.task_id() | :genesis
+    end
   end
 end
