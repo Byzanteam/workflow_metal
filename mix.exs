7abc41fedadd57689951e511941891131eade681
Phil Chen <06fahchen@gmail.com>
2020-07-10 10:16:39 +0800

    feat: introduce typed_struct

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule WorkflowMetal.MixProject do
       {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:doctor, "~> 0.15.0", only: [:dev]},
-      {:gen_state_machine, "~> 2.0"}
+      {:gen_state_machine, "~> 2.0"},
+      {:typed_struct, "~> 0.2.0"}
     ]
   end
 
