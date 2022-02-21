62b3f8a53421347856cc2a16416ac08f47c503d6
Phil Chen <06fahchen@gmail.com>
2022-02-21 21:47:05 +0800

    chore: bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WorkflowMetal.MixProject do
   def project do
     [
       app: :workflow_metal,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
